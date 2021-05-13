b37c0275acea8fb7497b5eea08f7ab0058750510
VanshMt <83874790+VanshMt@users.noreply.github.com>
2021-05-13 16:23:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # From the Discord Developer Portal, copy your bot's token and paste it between the quotation marks.
 # You can also change the command prefix if needed. Default is !
 
-token=''
-prefix='!'
+token='ODQyMTI5MDMwMDcyODkzNDYw.YJwzxg.9c2WSgxLVHzUD3GTKIQ_ZRagkqE'
+prefix='@'
